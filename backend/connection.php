edad5fb245a2f587b686e0d9c4db895da483df7d
MarioGoDevLike <marionassar57@gmail.com>
2024-01-03 18:11:38 +0200

    zxx

--- backend/connection.php
+++ backend/connection.php
@@ -1,3 +1,3 @@
 <?php 
-$con = mysqli_connect("localhost", "root", "", "ecommercestore"); 
+$con = mysqli_connect("monorail.proxy.rlwy.net", "root", "6C2-5dbHDbhgG2aGbAcebgBeC-FeGDdh", "railway"); 
 ?>
